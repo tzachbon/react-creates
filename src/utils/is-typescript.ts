ca8b4e60c48ab6773a3e6d36402bf86aba4b270a
tzachbon <tzachb@wix.com>
2020-07-17 11:56:25 +0300

    fix(is-ts): trying to require typescript

--- src/utils/is-typescript.ts
+++ src/utils/is-typescript.ts
@@ -1,16 +1,10 @@
-import { exists as existsCb } from "fs";
-import { promisify } from "util";
-import { join } from "path";
-
-const exists = promisify(existsCb);
-
-let __isTypescriptCache: boolean;
-
-const _isTypescript = async (target: string) => {
-  __isTypescriptCache = await exists(join(target, "tsconfig.json"));
-  return __isTypescriptCache;
-};
-
 export default async function isTypescript(target = process.cwd()) {
-  return __isTypescriptCache ?? (await _isTypescript(target));
+  let isInsideTypescript;
+  try {
+    isInsideTypescript = !!require("typescript");
+  } catch (e) {
+    isInsideTypescript = false;
+  }
+
+  return isInsideTypescript;
 }
