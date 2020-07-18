4e34e95da0f2f6bb3e13f986d0fd1806603bc0a3
tzachbon <tzachb@wix.com>
2020-07-18 18:29:33 +0300

    create `create-react-app` mocks

--- __tests__/react-creates.test.ts
+++ __tests__/react-creates.test.ts
@@ -3,11 +3,11 @@ import { tempProjectTestkit } from "./driver/create-mini-project.testkit";
 describe("react-creates", () => {
   const driver = tempProjectTestkit();
 
-  beforeEach(async () => await driver.start());
-  afterEach(async () => await driver.reset());
+  beforeAll(async () => await driver.start());
+  afterAll(async () => await driver.reset());
 
-  it("should create component (happy flow)",async  () => {
-    console.log(await driver.createComponent("__Test__"));
+  it("should create component (happy flow)", async () => {
+    // console.log(await driver.createComponent("__Test__"));
 
     expect(true).toBe(true);
   });
