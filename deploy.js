107adb4d324f5300a4e31a50386bd9d69a915b8b
tzachbon <tzachb@wix.com>
2020-07-18 18:11:11 +0300

    remove fs/promises

--- deploy.js
+++ deploy.js
@@ -1,8 +1,11 @@
 const execa = require("execa");
 const { program } = require("commander");
 const packageJson = require("./package");
-const { writeFile } = require("fs/promises");
+const fs = require("fs");
+const { promisify } = require("util");
 const { resolve } = require("path");
+
+const writeFile = promisify(fs.writeFile);
 
 const updateIndex = {
   major: 0,
@@ -37,19 +40,27 @@ program
         resolve("package.json"),
         JSON.stringify(packageJson, null, 2)
       );
+      console.log('### Updating package.json');
+      
 
       // publish current version
       await execa("npm", ["publish"]);
+      console.log('### Publishing to npm');
 
       // update git with new version
       await execa("git", ["add", "package.json"]);
+      console.log('### Git: adding package.json');
       await execa("git", [
         "commit",
         "-m",
         `":tada: New version created!" ${newVersion}`,
       ]);
+      console.log('### Git: committing');
       await execa("git", ["push"]);
+      console.log('### Git: pushing 🥳');
+
     } catch (e) {
+      console.error(e);
       throw e;
     }
   });
