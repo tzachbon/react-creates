06f0b149885cd498f852797d9180a89e05a3f665
tzachbon <tzachb@wix.com>
2020-08-07 15:57:47 +0300

    fix(cli): handle not exists dir in `getPackageJson`

--- packages/react-creates/src/utils/get-package-json.ts
+++ packages/react-creates/src/utils/get-package-json.ts
@@ -3,6 +3,7 @@ import { join, sep } from "path";
 import { promisify } from "util";
 
 const readdir = promisify(fs.readdir)
+const exists = promisify(fs.exists)
 
 export type PackageJsonType = Record<string, IPackageJson | string | number | boolean>
 
@@ -10,16 +11,25 @@ interface IPackageJson extends PackageJsonType { };
 
 export default async function getPackageJson(
   {
-    cwd = process.cwd()
+    cwd = process.cwd(),
+    depth = 10
   }
 ): Promise<PackageJsonType | null> {
 
   const directories = cwd.split(sep);
   const PACKAGE_JSON = `package.json`;
 
-  while (directories.length) {
+  let iterations = 0;
+
+  while (directories.length && iterations < depth) {
+
+    iterations++;
+
     const directory = directories.pop()
     const path = join(sep, ...directories, directory)
+
+    if (!(await exists(path))) continue;
+
     const dirFiles = await readdir(path)
 
     if (dirFiles.includes(PACKAGE_JSON)) {
