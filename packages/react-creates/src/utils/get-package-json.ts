dffe29b25f3ec315c855ead532f775adc42fc6aa
tzachbon <tzachb@wix.com>
2020-08-07 13:15:55 +0300

    feat(util): get nearest package json

---
+++ packages/react-creates/src/utils/get-package-json.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import { join, sep } from "path";
+import { promisify } from "util";
+
+const readdir = promisify(fs.readdir)
+
+export type PackageJsonType = Record<string, IPackageJson | string | number | boolean>
+
+interface IPackageJson extends PackageJsonType { };
+
+export default async function getPackageJson(): Promise<PackageJsonType | null> {
+
+  const cwd = process.cwd();
+  const directories = cwd.split(sep);
+  const PACKAGE_JSON = `package.json`;
+
+  while (directories.length) {
+    const directory = directories.pop()
+    const path = join(sep, ...directories, directory)
+    const dirFiles = await readdir(path)
+
+    if (dirFiles.includes(PACKAGE_JSON)) {
+      const packageJson = require(join(path, PACKAGE_JSON)) as PackageJsonType | null;
+
+      if (!packageJson) {
+        continue;
+      }
+
+      return packageJson;
+    }
+  }
+
+  return null;
+}
+
