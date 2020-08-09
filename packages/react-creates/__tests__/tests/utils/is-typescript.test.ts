1c56eeddf31ef80b967cfdffe26829c71ec7de85
tzachbon <tzachb@wix.com>
2020-08-09 09:29:33 +0300

    test(cli): `isTypescript` full coverage

--- packages/react-creates/__tests__/tests/utils/is-typescript.test.ts
+++ packages/react-creates/__tests__/tests/utils/is-typescript.test.ts
@@ -1,57 +1,42 @@
 import mockFs from 'mock-fs';
 import isTypescript from '../../../src/utils/is-typescript';
 
-const fsMockDriver = ({ packageJson = null, tsConfig = null } = {}) => {
+const mockFsDriver = ({
+  packageJson = null,
+  tsConfig = null,
+  removeTsconfig = false,
+  removePackageJson = false
+} = {}) => {
 
   let options = { packageJson, tsConfig }
 
-  const start = () => mockFs({
-    'main': {
-      'tsconfig.json': options.tsConfig ?? `{ }`,
-      'package.json': options.packageJson ?? `
+  const start = () => {
+    const mock = {
+      'main': {
+        'src': {
+          'App.tsx': `import React from 'react'`
+        }
+      }
+    }
+
+    if (!removeTsconfig) {
+      mock.main['tsconfig.json'] = options.tsConfig ?? `{ }`;
+    }
+
+    if (!removePackageJson) {
+      mock.main['package.json'] = options.packageJson ?? `
       {
-          "version": "1.0.0",
-          "scripts": {
-            "test": "npx jest"
-          },
           "dependencies": {
             "typescript": "3.8.0"
           },
           "devDependencies": {
             "typescript": "3.8.0"
           }
-      }`,
-      'src': {
-        'App.tsx': `import React from 'react';
-            import logo from './logo.svg';
-            import './App.css';
-            
-            function App() {
-              return (
-                <div className="App">
-                  <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                      Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                      className="App-link"
-                      href="https://reactjs.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Learn React
-                    </a>
-                  </header>
-                </div>
-              );
-            }
-            
-            export default App;
-            `
-      }
+      }`
     }
-  })
+
+    mockFs(mock);
+  }
 
   const reset = () => mockFs.restore();
 
@@ -60,7 +45,7 @@ const fsMockDriver = ({ packageJson = null, tsConfig = null } = {}) => {
     afterEach(reset);
   }
 
-  const updateProps = (newOptions: any) => {
+  const updateProps = (newOptions: any = {}) => {
     options = {
       ...options,
       ...newOptions
@@ -83,14 +68,68 @@ const fsMockDriver = ({ packageJson = null, tsConfig = null } = {}) => {
 }
 
 describe('Is Typescript', () => {
-  const driver = fsMockDriver();
+  const driver = mockFsDriver();
+  const target = driver.getTarget();
 
   driver.beforeAndAfter();
 
   it('should return true if has tsconfig and dependencies', async () => {
-    const target = driver.getTarget();
     expect(await isTypescript(target)).toBe(true)
+  })
 
+  it('should return true if has only dependencies', async () => {
+    driver.updateProps({
+      packageJson: `
+      {
+        "dependencies": {
+          "typescript": "3.8.0"
+        }
+      }`
+    });
+    expect(await isTypescript(target)).toBe(true)
+  })
+
+  it('should return true if has only devDependencies', async () => {
+    driver.updateProps({
+      packageJson: `
+      {
+        "devDependencies": {
+          "typescript": "3.8.0"
+        }
+      }`
+    });
+    expect(await isTypescript(target)).toBe(true)
+  })
+
+  it('should return false if doesn\'t have typescript in dependencies', async () => {
+    driver.updateProps({
+      packageJson: `
+      {
+        "devDependencies": {}
+      }`
+    });
+    expect(await isTypescript(target)).toBe(false)
+
+    driver.updateProps({
+      packageJson: `
+      {
+        "dependencies": {}
+      }`
+    });
+    expect(await isTypescript(target)).toBe(false)
+  })
+
+  it('should return false if doesn\'t have package.json', async () => {
+    driver.updateProps({ removePackageJson: true });
+
+    expect(await isTypescript(target)).toBe(false)
+  })
+
+
+  it('should return false if doesn\'t have tsconfig.json', async () => {
+    driver.updateProps({ removeTsconfig: true });
+
+    expect(await isTypescript(target)).toBe(false)
   })
 
 
