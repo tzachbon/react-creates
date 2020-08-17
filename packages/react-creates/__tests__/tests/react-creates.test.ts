f78c299d032068c4b042c2e3ddd350512c0e1645
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2020-08-17 10:20:05 +0300

    Handle invalid project (#14)

--- packages/react-creates/__tests__/tests/react-creates.test.ts
+++ packages/react-creates/__tests__/tests/react-creates.test.ts
@@ -1,9 +1,16 @@
+import mockFs from 'mock-fs';
+import tempy from 'tempy';
+import { componentTestkit } from "../testkit/component.testkit";
 import { tempProjectTestkit } from "../testkit/create-react-app.testkit";
 
 describe("react-creates", () => {
   const driver = tempProjectTestkit({ install: true });
 
   driver.beforeAndAfter();
+
+  beforeEach(() => {
+    mockFs.restore()
+  })
 
   it("should create component (happy flow)", async () => {
     const cmpName = `TestCmp`;
@@ -21,4 +28,10 @@ describe("react-creates", () => {
 
     expect(isIncludesSuccessMessage).toBe(true);
   });
+
+  it('should fail for not having react', async () => {
+
+    const componentDriver = componentTestkit('SomeComponent', { target: tempy.directory() })
+    await expect(componentDriver.create()).rejects.toHaveProperty('stderr')
+  })
 });
