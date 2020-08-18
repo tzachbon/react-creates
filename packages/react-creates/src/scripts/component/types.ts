c55b5c8277fbd83b8743d6fddbb720c4c4ca08ec
tzachbon <tzachb@wix.com>
2020-08-18 14:19:24 +0300

    feat(cli): Can skip test files with: `--skip-test`

--- packages/react-creates/src/scripts/component/types.ts
+++ packages/react-creates/src/scripts/component/types.ts
@@ -10,4 +10,5 @@ export interface CreateComponentOptions {
   style: Styles;
   entry: boolean;
   propTypes: boolean;
+  skipTest: boolean
 }
--- packages/react-creates/src/utils/copy-template.ts
+++ packages/react-creates/src/utils/copy-template.ts
@@ -1,26 +1,47 @@
-import { exists as existsCb, mkdir as mkdirCb } from "fs";
-import npc from "ncp";
-import { sep } from "path";
-import { promisify } from "util";
+import fs from 'fs';
+import npc, { Options } from 'ncp';
+import { sep } from 'path';
+import { promisify } from 'util';
+import { CreateComponentOptions } from '../scripts/component/types';
 
 const copy = promisify(npc);
-const exists = promisify(existsCb);
-const mkdir = promisify(mkdirCb);
+const exists = promisify(fs.exists);
+const mkdir = promisify(fs.mkdir);
 
-const isTemp = process.env.TEMP === "true";
-const shouldAddTemp = (path) => path + (isTemp ? sep + "__temp" : "");
+const isDir = (file: string) =>
+  fs.lstatSync(file).isDirectory();
+const isTemp = process.env.TEMP === 'true';
+const shouldAddTemp = (path) =>
+  path + (isTemp ? sep + '__temp' : '');
 
 export const copyTemplate = async (
   templatesPath: string,
-  targetComponentPath: string
+  targetComponentPath: string,
+  options: CreateComponentOptions
 ) => {
-  const targetComponentPathExists = await exists(targetComponentPath);
+  const targetComponentPathExists = await exists(
+    targetComponentPath
+  );
 
   if (!targetComponentPathExists) {
     await mkdir(targetComponentPath);
   }
 
-  await copy(templatesPath, shouldAddTemp(targetComponentPath), {
+  const copyOptions: Options = {
     clobber: true,
-  });
+  };
+
+  if (options.skipTest) {
+    copyOptions.filter = (file) =>
+      isDir(file) ||
+      /^(?!.*\.test\.(js|ts)$).*/.test(
+        file
+      );
+  }
+
+  await copy(
+    templatesPath,
+    shouldAddTemp(targetComponentPath),
+    copyOptions
+  );
 };
