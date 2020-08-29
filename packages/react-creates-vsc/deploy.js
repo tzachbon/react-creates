c9ddb4a51c247912bd37c27978dc61acb31df773
tzachbon <tzachb@wix.com>
2020-08-29 16:06:11 +0300

    dev(vsc): deploy script

--- packages/react-creates-vsc/deploy.js
+++ packages/react-creates-vsc/deploy.js
@@ -1,5 +1,17 @@
 const execa = require('execa');
 const packageJson = require('./package.json');
 
-execa.sync('git', ['add', 'package.json']);
-execa.sync('git', ['commit', '-m', `vsc: ${packageJson.version}`]);
+const exe = ([first, ...args]) => execa(first, [...args]).stdout.pipe(process.stdout);
+
+async function main() {
+  await exe(['npm', 'i', 'react-creates@latest']);
+  await exe(['npx', 'vsce', 'publish', 'patch']);
+  await exe(['git', 'add', 'package.json']);
+  await exe(['git', 'commit', '-m', `vsc: ${packageJson.version}`]);
+}
+
+try {
+  main();
+} catch (e) {
+  throw e;
+}
