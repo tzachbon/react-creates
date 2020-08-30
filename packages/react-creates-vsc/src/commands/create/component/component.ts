8d9bbb7416e7fff43e8712ae1751dd072afce4a4
tzachbon <tzachb@wix.com>
2020-08-30 21:38:05 +0300

    fix(vsc): windows path

--- packages/react-creates-vsc/src/commands/create/component/component.ts
+++ packages/react-creates-vsc/src/commands/create/component/component.ts
@@ -1,19 +1,24 @@
 import * as vscode from 'vscode';
 import ReactCreates from '../../../utils/react-creates';
 import { getPath } from '../../../utils/get-path';
+import { Optional } from 'utility-types';
 
 export default {
   name: 'react-creates-vsc.component',
-  command: async ({ path = getPath() } = {}) => {
+  command: async ({
+    path,
+    ...rest
+  }: { path?: string | undefined; _fsPath?: string | undefined } = {}) => {
+    
+    path ||= rest._fsPath;
+
     if (!path) {
       return vscode.window.showErrorMessage('Path is not found');
     }
 
     try {
-
       const reactCreates = await ReactCreates.start(path);
       await reactCreates.createComponent();
-      
     } catch (error) {
       vscode.window.showErrorMessage(
         error?.message || 'Something went wrong with the extension :( Please try again'
