b74d794ffbba5cba0a0c8195b84f5e538851083f
tzachbon <tzachb@wix.com>
2020-10-27 16:45:30 +0200

    Stale package deploy

--- packages/react-creates/deploy.js
+++ packages/react-creates/deploy.js
@@ -1,85 +1,87 @@
-const { program } = require("commander");
-const packageJson = require("./package");
-const fs = require("fs");
-const { promisify } = require("util");
-const { resolve } = require("path");
-const chalk = require("chalk");
-const execa = require("execa");
+// const { program } = require("commander");
+// const packageJson = require("./package");
+// const fs = require("fs");
+// const { promisify } = require("util");
+// const { resolve } = require("path");
+// const chalk = require("chalk");
+// const execa = require("execa");
 
-const writeFile = promisify(fs.writeFile);
+// const writeFile = promisify(fs.writeFile);
 
-const updateIndex = {
-  major: 0,
-  minor: 1,
-  patch: 2,
-};
+// const updateIndex = {
+//   major: 0,
+//   minor: 1,
+//   patch: 2,
+// };
 
-program
-  .option("-m --minor")
-  .option("-p --patch")
-  .option("-t --tag")
-  .option("-g --git")
-  .action(async (_) => {
-    try {
-      const { major, minor, patch, git, tag } = _.opts();
+// program
+//   .option("-m --minor")
+//   .option("-p --patch")
+//   .option("-t --tag")
+//   .option("-g --git")
+//   .action(async (_) => {
+//     try {
+//       const { major, minor, patch, git, tag } = _.opts();
 
-      const version = packageJson.version.split(".").map((_) => parseInt(_));
-      const message = `New react-scripts version: \`${packageJson.version}\``;
+//       const version = packageJson.version.split(".").map((_) => parseInt(_));
+//       const message = `New react-scripts version: \`${packageJson.version}\``;
 
-      if (tag) {
-        await execa("git", ["tag", `v${packageJson.version}`]);
-      }
+//       if (tag) {
+//         await execa("git", ["tag", `v${packageJson.version}`]);
+//       }
 
-      if (git) {
-        await execa("git", ["add", "."]);
-        await execa("git", ["commit", "-m", message]);
-        await execa("git", ["push"]);
+//       if (git) {
+//         await execa("git", ["add", "."]);
+//         await execa("git", ["commit", "-m", message]);
+//         await execa("git", ["push"]);
 
-        console.log(
-          chalk.green(
-            `
-            Git commit created successfully
-          `
-          )
-        );
+//         console.log(
+//           chalk.green(
+//             `
+//             Git commit created successfully
+//           `
+//           )
+//         );
 
-        return;
-      }
+//         return;
+//       }
 
-      let versionType;
-      for (const [versionKey, shouldUpdate] of Object.entries({
-        major,
-        minor,
-        patch,
-      })) {
-        if (shouldUpdate) {
-          version[updateIndex[versionKey]]++;
-          versionType = versionKey.charAt(0);
-        }
-      }
+//       let versionType;
+//       for (const [versionKey, shouldUpdate] of Object.entries({
+//         major,
+//         minor,
+//         patch,
+//       })) {
+//         if (shouldUpdate) {
+//           version[updateIndex[versionKey]]++;
+//           versionType = versionKey.charAt(0);
+//         }
+//       }
 
-      const newVersion = version.join(".");
-      packageJson.version = newVersion;
+//       const newVersion = version.join(".");
+//       packageJson.version = newVersion;
 
-      // update package.json
-      await writeFile(
-        resolve("package.json"),
-        JSON.stringify(packageJson, null, 2)
-      );
-      console.log(
-        chalk.green(
-          "### Updated package.json, version: " + chalk.bold(newVersion)
-        )
-      );
+//       // update package.json
+//       await writeFile(
+//         resolve("package.json"),
+//         JSON.stringify(packageJson, null, 2)
+//       );
+//       console.log(
+//         chalk.green(
+//           "### Updated package.json, version: " + chalk.bold(newVersion)
+//         )
+//       );
 
-      if (versionType) {
-        await execa("npm", ["run", "version", `-${versionType}`]);
-        console.log(chalk.blueBright`Change log updated 🕓`);
-      }
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  });
+//       if (versionType) {
+//         await execa("npm", ["run", "version", `-${versionType}`]);
+//         console.log(chalk.blueBright`Change log updated 🕓`);
+//       }
+//     } catch (e) {
+//       console.error(e);
+//       throw e;
+//     }
+//   });
 
-program.parse(process.argv);
+// program.parse(process.argv);
+
+console.log('Try to deploy package')
