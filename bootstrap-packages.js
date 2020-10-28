e44476290b2ec67e649aebea977bcb06bd056cf7
tzachbon <tzachb@wix.com>
2020-10-28 10:12:00 +0200

    Create bootstrap packages script

---
+++ bootstrap-packages.js
@@ -0,0 +1,21 @@
+const fs = require('fs/promises');
+const path = require('path');
+const execa = require('execa');
+const isCi = require('is-ci');
+
+(async () => {
+  if (isCi) {
+    const packageBasePath = path.join(__dirname, 'packages');
+    const packagesNames = await fs.readdir(packageBasePath);
+    const packagesPathes = packagesNames.map(name => path.join(packageBasePath, name));
+
+    for (const packagePath of packagesPathes) {
+      execa('lerna', ['bootstrap', '--hoist'], { cwd: packagePath })
+        .stdout
+        .pipe(process.stdout)
+    }
+  } else {
+    console.log('### This script works only in CI');
+    
+  }
+})()
