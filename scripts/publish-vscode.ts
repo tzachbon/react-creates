3430dbcfc2a2ecc674c53857433c44b624140fbf
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2022-04-10 18:06:05 +0300

    infra: fix publish vscode exit code

--- scripts/publish-vscode.ts
+++ scripts/publish-vscode.ts
@@ -3,8 +3,13 @@ import { dirname } from 'path';
 
 const extensionRootDir = dirname(require.resolve('react-creates-vscode/package.json'));
 
-fork(require.resolve('vsce/vsce'), ['publish', '--yarn'], {
+const child = fork(require.resolve('vsce/vsce'), ['publish', '--yarn'], {
   cwd: extensionRootDir,
   env: process.env,
   stdio: 'inherit',
+});
+
+child.on('error', (e) => {
+  console.error(e);
+  process.exitCode = 1;
 });
