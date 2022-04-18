2b73ad2b2d4dbc1968c206f007420ebd9f77fb10
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2022-04-18 12:10:38 +0300

    fix(vscode): leaner publish process

--- scripts/publish-vscode.ts
+++ scripts/publish-vscode.ts
@@ -1,29 +1,18 @@
-import { fork, spawnSync } from 'child_process';
+import { fork } from 'child_process';
 import { dirname } from 'path';
 
 const extensionPackageJsonPath = require.resolve('react-creates-vsc/package.json');
 const extensionRootDir = dirname(extensionPackageJsonPath);
-const extensionId = 'TzachBonfil.react-creates-vsc';
 const vsce = require.resolve('vsce/vsce');
 
-const extensionData = JSON.parse(spawnSync(vsce, ['show', extensionId, '--json']).stdout.toString());
-const latestVersion = extensionData.versions[0] as { version: string; flags: number; lastUpdated: string };
-const extensionCurrentVersion = require(extensionPackageJsonPath).version as string;
+const child = fork(vsce, ['publish', '--yarn'], {
+  cwd: extensionRootDir,
+  env: process.env,
+  stdio: 'inherit',
+});
 
-if (latestVersion.version !== extensionCurrentVersion) {
-  console.log(`Updating extension from ${latestVersion.version} to ${extensionCurrentVersion}`);
-
-  const child = fork(vsce, ['publish', '--yarn'], {
-    cwd: extensionRootDir,
-    env: process.env,
-    stdio: 'inherit',
-  });
-
-  child.on('error', (e) => {
-    console.log(`Failed to update the extension.`);
-    console.error(e);
-    process.exitCode = 1;
-  });
-} else {
-  console.log(`"${extensionId}" is up to date (${extensionCurrentVersion})`);
-}
+child.on('error', (e) => {
+  console.log(`Failed to publish the extension.`);
+  console.error(e);
+  process.exitCode = 1;
+});
