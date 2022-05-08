14f3ca3375b53f75f755797af92463153a006e56
tzachbon <tzachbonfil@gmail.com>
2022-05-08 18:44:43 +0300

    test: check status code

--- packages/cli/test/driver.ts
+++ packages/cli/test/driver.ts
@@ -64,7 +64,7 @@ export class CliDriver {
 
     const childProcess = runCliSync(args, this.tempDirectory.path);
 
-    if (childProcess.error) {
+    if (childProcess.status === 0) {
       if (shouldThrow) {
         throw childProcess.error;
       } else {
